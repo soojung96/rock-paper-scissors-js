ebda1f1a0f99512bef5d826e74c7e84bbbc56b2c
Crystal Kim <crystalkim@Crystals-MacBook-Air.local>
2024-07-16 06:35:21 -0700

    finished rock paper scissors game

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
-function computerPlay() {
-    const options = ['Rock', 'Paper', 'Scissors'];
+function getComputerChoice() {
+    const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random()*options.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
-    if (playerSelection == computerSelection) {
-        return `It's a tie! ${playerSelection} does not beat ${playerSelection}`;
+function getHumanChoice() {
+    choice = prompt("Welcome to a game of Rock Paper Scissors! Please choose rock, paper, or scissors.");
+    return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+    if (humanChoice == computerChoice) {
+        console.log(`It's a tie! You and the computer both chose ${humanChoice}`);
+        return;
     }
-    const result = playerSelection + " " + computerSelection;
+    const result = humanChoice + " " + computerChoice;
     const winConditions = {
-        'Rock Scissors': true,
-        'Rock Paper': false,
-        'Paper Rock': true,
-        'Paper Scissors': false,
-        'Scissors Paper': true,
-        'Scissors Rock': false
+        'rock scissors': true,
+        'rock paper': false,
+        'paper rock': true,
+        'paper scissors': false,
+        'scissors paper': true,
+        'scissors rock': false
     };
     const win = winConditions[result];
     if (win) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        return 'player win';
+
     }
     else if (!win) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        return 'player loss'
     }
 }
-const playerSelection = 'rOck';
-const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
 
-function game() {
-    let currentRound = 1;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerWin = playRound(getHumanChoice(), getComputerChoice());
+        if (playerWin == 'player win') {
+            humanScore++;
+        }
+        else if (playerWin == 'player loss') {
+            computerScore++;
+        }
+    }
+    if (humanScore == computerScore) {
+        return `Nobody won the game! It was a tie! Both you and the computer won ${humanScore} times.`;
+    } else if (humanScore > computerScore) {
+        return `Congratulations, you won the game! You won ${humanScore} times to the computer's ${computerScore}.`;
+    } else if (computerScore > humanScore) {
+        return `Sorry, you lost the game! The computer won ${computerScore} times to your ${humanScore}.`;
+    }
 }
+
+console.log(playGame());
